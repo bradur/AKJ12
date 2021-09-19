e970c6c7f7bfda50a2d30916ce57a3eb25c39b2f
Juutis <Juutis>
2021-09-19 18:20:00 +0300

    Added shotgun

--- Game/Assets/Scripts/Character/Gun.cs
+++ Game/Assets/Scripts/Character/Gun.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     [SerializeField]
+    private int bulletsPerShot = 1;
+
+    [SerializeField]
     private Transform Muzzle;
 
     [SerializeField]
     private ParticleSystem HitEffect;
+    private List<ParticleSystem> hitEffects = new List<ParticleSystem>();
 
     [SerializeField]
     private ParticleSystem BloodEffect;
@@ -39,6 +43,12 @@ public class Gun : MonoBehaviour
         targetLayerMask = LayerMask.GetMask(targetLayers) | defaultMask;
         minDamage = config.MinDamage;
         maxDamage = config.MaxDamage;
+
+        for (var i = 0; i < bulletsPerShot; i++)
+        {
+            var effect = Instantiate(HitEffect, transform);
+            hitEffects.Add(effect);
+        }
     }
 
     // Start is called before the first frame update
@@ -57,58 +67,66 @@ public class Gun : MonoBehaviour
     {
         if (readyToFire)
         {
-            var inaccuracy = Random.Range(-config.Inaccuracy, config.Inaccuracy);
-            var inaccuracyAdjustment = Quaternion.AngleAxis(inaccuracy, Vector3.forward);
-
-            var targetDirection = inaccuracyAdjustment * Muzzle.up;
-            var hit = Physics2D.Raycast(Muzzle.position, targetDirection, config.Range, targetLayerMask);
-            if (hit.collider != null)
+            for (var i = 0; i < bulletsPerShot; i++)
             {
-                bool effectPlayed = false;
-                var character = hit.collider.GetComponent<Character>();
-                if (character != null)
+                var shotgunSign = i % 2 == 0 ? 1 : -1;
+                var shotgunAngle = shotgunSign * ((i+1)/2) * 15f;
+                var shotgunAdjustment = Quaternion.AngleAxis(shotgunAngle, Vector3.forward);
+                var baseDir = shotgunAdjustment * Muzzle.up;
+
+                var inaccuracy = Random.Range(-config.Inaccuracy, config.Inaccuracy);
+                var inaccuracyAdjustment = Quaternion.AngleAxis(inaccuracy, Vector3.forward);
+
+                var targetDirection = inaccuracyAdjustment * baseDir;
+                var hit = Physics2D.Raycast(Muzzle.position, targetDirection, config.Range, targetLayerMask);
+                if (hit.collider != null)
                 {
-                    var damage = Random.Range(minDamage, maxDamage);
-                    character.Hurt(damage);
-                    if (hit.collider.gameObject.tag == "Player")
+                    bool effectPlayed = false;
+                    var character = hit.collider.GetComponent<Character>();
+                    if (character != null)
                     {
-                        PostProcessingEffects.Main.PlayerDamaged();
-                        BloodEffect.transform.position = hit.point;
-                        BloodEffect.Play();
-                        effectPlayed = true;
-                    } else {
-                        SoundManager.main.PlaySound(GameSoundType.RobotHit);
-                        PostProcessingEffects.Main.EnemyDamaged(hit.point);
+                        var damage = Random.Range(minDamage, maxDamage);
+                        character.Hurt(damage);
+                        if (hit.collider.gameObject.tag == "Player")
+                        {
+                            PostProcessingEffects.Main.PlayerDamaged();
+                            BloodEffect.transform.position = hit.point;
+                            BloodEffect.Play();
+                            effectPlayed = true;
+                        } else {
+                            SoundManager.main.PlaySound(GameSoundType.RobotHit);
+                            PostProcessingEffects.Main.EnemyDamaged(hit.point);
+                        }
+                    }
+
+                    if (!effectPlayed)
+                    {
+                        hitEffects[i].transform.position = hit.point;
+                        hitEffects[i].Play();
                     }
                 }
 
-                if (!effectPlayed)
+                readyToFire = false;
+                Invoke("ReadyToFire", 1.0f / config.FireRate);
+
+                var trail = Instantiate(BulletTrail, Muzzle.position, Quaternion.identity);
+                trail.AddPosition(Muzzle.position);
+                if (hit.collider != null)
                 {
-                    HitEffect.transform.position = hit.point;
-                    HitEffect.Play();
+                    trail.transform.position = hit.point;
                 }
-            }
+                else
+                {
+                    trail.transform.position = Muzzle.position + targetDirection.normalized * config.Range;
+                }
 
-            readyToFire = false;
-            Invoke("ReadyToFire", 1.0f / config.FireRate);
+                MuzzleFlash.Play();
+                MuzzleLight.Flash();
 
-            var trail = Instantiate(BulletTrail, Muzzle.position, Quaternion.identity);
-            trail.AddPosition(Muzzle.position);
-            if (hit.collider != null)
-            {
-                trail.transform.position = hit.point;
-            }
-            else
-            {
-                trail.transform.position = Muzzle.position + targetDirection.normalized * config.Range;
-            }
-
-            MuzzleFlash.Play();
-            MuzzleLight.Flash();
-
-            if (anim != null)
-            {
-                anim.Play("Base Layer.Shoot", 0, 0.25f);
+                if (anim != null)
+                {
+                    anim.Play("Base Layer.Shoot", 0, 0.25f);
+                }
             }
         }
     }
